e24daf6b6e9ac51bac9deb79dcff78544ff7af81
joseluisvides71-ctrl <joseluisvides71@gmail.com>
2026-06-03 20:04:08 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { mochaPlugins } from "@getmocha/vite-plugins";
 
-export default defineConfig({
+export default defineConfig({base: './',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
   server: {
